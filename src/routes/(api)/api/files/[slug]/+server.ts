17807c23367801ab59cf0ae94828f569bf16f494
Paul Rodriguez <mildmoose@pm.me>
2024-04-04 13:07:09 -0600

    Changed req to api to be client side

--- src/routes/(api)/api/files/[slug]/+server.ts
+++ src/routes/(api)/api/files/[slug]/+server.ts
@@ -1,99 +1,99 @@
-import { json, error } from '@sveltejs/kit'
-import { PUBLIC_API_URL } from '$env/static/public';
-import type { File, DefaultApiResponse } from '$lib/types/api';
+// import { json, error } from '@sveltejs/kit'
+// import { PUBLIC_API_URL } from '$env/static/public';
+// import type { File, DefaultApiResponse } from '$lib/types/api';
 
-export const GET = async ({ fetch, cookies, params }) => {
-    try {
-        const auth = cookies.get("token")
-        if (!auth) error(401, {
-            message: "Auth not provided"
-        })
+// export const GET = async ({ fetch, cookies, params }) => {
+//     try {
+//         const auth = cookies.get("token")
+//         if (!auth) error(401, {
+//             message: "Auth not provided"
+//         })
 
-        const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
-            method: 'GET',
-            headers: {
-              Authorization: `Bearer ${auth}`
-            }
-        })
+//         const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
+//             method: 'GET',
+//             headers: {
+//               Authorization: `Bearer ${auth}`
+//             }
+//         })
 
-        if (res.status !== 200) {
-            throw new Error("Unexpected status code " + res.status)
-        }
-        const body: File = JSON.parse(JSON.stringify(await res.json()))
+//         if (res.status !== 200) {
+//             throw new Error("Unexpected status code " + res.status)
+//         }
+//         const body: File = JSON.parse(JSON.stringify(await res.json()))
 
-        return json(body)
-    } catch (err) {
-        await console.error(err)
-        error(500, {
-            message: `${err}`
-        })
-    }
-}
+//         return json(body)
+//     } catch (err) {
+//         await console.error(err)
+//         error(500, {
+//             message: `${err}`
+//         })
+//     }
+// }
 
-export const PATCH = async ({ fetch, cookies, request, params }) => {
-    try {
-        const auth = cookies.get("token")
-        if (!auth) error(401, {
-            message: "Auth not provided"
-        })
-        const reqBody = await request.json()
+// export const PATCH = async ({ fetch, cookies, request, params }) => {
+//     try {
+//         const auth = cookies.get("token")
+//         if (!auth) error(401, {
+//             message: "Auth not provided"
+//         })
+//         const reqBody = await request.json()
 
-        if (!reqBody.latex.includes("documentclass{article}")) {
-            reqBody.latex = `\\documentclass{article}\\begin{document}${reqBody.latex}\\end{document}`
-        }
-        await console.log(reqBody)
+//         if (!reqBody.latex.includes("documentclass{article}")) {
+//             reqBody.latex = `\\documentclass{article}\\begin{document}${reqBody.latex}\\end{document}`
+//         }
+//         await console.log(reqBody)
 
-        const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
-            method: 'PATCH',
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${auth}`
-            },
-            body: JSON.stringify(reqBody)
-        })
+//         const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
+//             method: 'PATCH',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization: `Bearer ${auth}`
+//             },
+//             body: JSON.stringify(reqBody)
+//         })
 
-        const body = await res.json()
-        await console.log(body)
-        if (res.status !== 200) {
-            throw new Error("Unexpected status code " + res.status)
-        }
-        // const body: File = JSON.parse(JSON.stringify(await res.json()))
+//         const body = await res.json()
+//         await console.log(body)
+//         if (res.status !== 200) {
+//             throw new Error("Unexpected status code " + res.status)
+//         }
+//         // const body: File = JSON.parse(JSON.stringify(await res.json()))
 
-        return json({success: true})
-    } catch (err) {
-        await console.error(err)
-        error(500, {
-            message: `${err}`
-        })
-    }
-}
+//         return json({success: true})
+//     } catch (err) {
+//         await console.error(err)
+//         error(500, {
+//             message: `${err}`
+//         })
+//     }
+// }
 
-export const DELETE = async ({ fetch, cookies, params, url }) => {
-    try {
-        const auth = cookies.get("token")
-        if (!auth) error(401, {
-            message: "Auth not provided"
-        })
-        await console.log(`${PUBLIC_API_URL}/files/${params.slug}`, params)
-        const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
-            method: 'DELETE',
-            headers: {
-              Authorization: `Bearer ${auth}`,
-            }
-        })
+// export const DELETE = async ({ fetch, cookies, params, url }) => {
+//     try {
+//         const auth = cookies.get("token")
+//         if (!auth) error(401, {
+//             message: "Auth not provided"
+//         })
+//         await console.log(`${PUBLIC_API_URL}/files/${params.slug}`, params)
+//         const res = await fetch(`${PUBLIC_API_URL}/files/${params.slug}`, {
+//             method: 'DELETE',
+//             headers: {
+//               Authorization: `Bearer ${auth}`,
+//             }
+//         })
 
-        if (res.status !== 200) {
-            throw new Error("Unexpected status code " + res.status)
-        }
-        const body: DefaultApiResponse = JSON.parse(JSON.stringify(await res.json()))
+//         if (res.status !== 200) {
+//             throw new Error("Unexpected status code " + res.status)
+//         }
+//         const body: DefaultApiResponse = JSON.parse(JSON.stringify(await res.json()))
 
-        return json(body, {
-            status: 200
-        })
-    } catch (err) {
-        await console.error(err)
-        error(500, {
-            message: `${err}`
-        })
-    }
-}
+//         return json(body, {
+//             status: 200
+//         })
+//     } catch (err) {
+//         await console.error(err)
+//         error(500, {
+//             message: `${err}`
+//         })
+//     }
+// }
